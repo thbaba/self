15e664ffdca08c9c97e89d7a328499e879addfad
Can Deniz <dcb.denizcan@gmail.com>
2025-08-14 03:15:16 +0300

    AnalysisService added

--- src/main/java/com/denizcanbagdatlioglu/self/analysis/domain/entity/Question.java
+++ src/main/java/com/denizcanbagdatlioglu/self/analysis/domain/entity/Question.java
@@ -6,7 +6,7 @@ public class Question {
 
     private Question(String q) {question = q;}
 
-    public Question of(String q) {return new Question(q);}
+    public static Question of(String q) {return new Question(q);}
 
     public String get() { return question; }
 
